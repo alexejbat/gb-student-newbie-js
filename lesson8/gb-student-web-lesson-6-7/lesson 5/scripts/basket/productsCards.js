7cb11651d96d90f519ff6437273b1ad57d7c61df
alexejbat <85104937+alexejbat@users.noreply.github.com>
2021-07-30 08:57:10 +0400

    lesson8
    добавлены продукты и скрипт

---
+++ lesson8/gb-student-web-lesson-6-7/lesson 5/scripts/basket/productsCards.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const pathToImages = 'img';
+const pathToProductsImages = `${pathToImages}`;
+const featuredItemsEl = document.querySelector('.featuredItem');
+
+
+function getProductMarkup(product) {
+    return `
+        <div class="featuredItems"> 
+        
+
+            <div class="featuredImgWrap">
+                <img src="${pathToProductsImages}/${product.image}" alt="${product.name}">
+                <div class="featuredImgDark">
+                    <button data-productId="${product.id}">
+                        <img src="${pathToImages}/cart.svg" alt="">
+                        Add to Cart
+                    </button>
+                </div>
+            </div>
+
+            <div class="featuredData">
+                <div class="featuredName">
+                    ${product.name}
+                </div>
+                <div class="featuredText">
+                    ${product.description}
+                </div>
+                <div class="featuredPrice">
+                    $${product.price}
+                </div>
+            </div>
+
+        </div>
+    `;
+}
+
+
+function insertProductsIntoPage(products, featuredItemsEl) {
+    let productsMarkup = '';
+    for (let product of products) {
+        productsMarkup += getProductMarkup(product);
+    }
+    featuredItemsEl.insertAdjacentHTML('afterbegin', productsMarkup);
+}
+
+
+function addEventListenersForAddToCartButtons() {
+    const addToCartBtns = document.querySelectorAll('button[data-productId]');
+    addToCartBtns.forEach(function (button) {
+        button.addEventListener('click', addedProductHandler);
+    })
+}
+
+
+function addedProductHandler(event) {
+    const productId = event.currentTarget.getAttribute('data-productId');
+    addProductIntoBasket(productId);
+}
+
+insertProductsIntoPage(products, featuredItemsEl);
+addEventListenersForAddToCartButtons();
