80ed16dc90559c800d98dc42a5a1bdd8cc05d4dd
alexejbat <alexejbat@yandex.ru>
2021-08-20 15:00:50 +0400

    урок 1 продвинутый курс джава
    добавлены стили

---
+++ gb-student-advanced-js/lesson1/backup/main.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const goods = [
+    {id: 1, title: 'Вобла', price: 900 },
+    {id: 2, title: 'Чехонь', price: 900},
+    {id: 3, title: 'Лещ', price: 550},
+    {id: 4, title: 'Сопа', price: 700},
+];
+
+const renderGoodsItem = (title, price) => {
+    return `<div class="goods-item">
+                <img src="img/ryba.jpg" alt="ryba">
+                <h3>${title}</h3>
+                <p>${price}</p>
+                <button class="buy-btn">Купить рыбки</button>           
+            </div>`;
+};
+  
+const renderGoodsList = (list) => {
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    document.querySelector('.goods-list').innerHTML = goodsList;
+}
+  
+renderGoodsList(goods);
