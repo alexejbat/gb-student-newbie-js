a2ad3141422a4e78c31181a393474cc658bf0000
alexejbat <alexejbat@yandex.ru>
2021-08-25 10:00:15 +0400

    урок 2 продвинутая верстка

---
+++ gb-student-advanced-js/lesson1/js/basket.js
@@ -0,0 +1,31 @@
+"use strict"
+
+class Basket{
+    constructor(container=".products-cart"){
+        this.sum = sum;
+        this.goods = goods;
+        this.container = container;
+        this.addProduct = addProduct;
+        this.deleteProduct = deleteProduct;
+        this.totalPrice = totalPrice;
+    } 
+
+    addtoBasket(){
+        
+    }
+
+    deletefromBasket(){
+
+    }
+
+    render(){
+        
+    }
+
+}
+
+class BasketItem{
+    render(){
+
+    }
+}
