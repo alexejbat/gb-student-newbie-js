6b9d60a950183e9146884de1b01a584d345f087e
alexejbat <alexejbat@yandex.ru>
2021-09-08 15:00:55 +0400

    low fix

--- gb-student-advanced-js/lesson1/js/main-ls4.js
+++ gb-student-advanced-js/lesson1/js/main-ls4.js
@@ -85,3 +85,65 @@ const app = new Vue({
 });
 
 
+//     class ProductList {
+//     constructor(container=".goods-list"){
+//         this.container = container;
+//         this.goods = [];
+//         this.allProducts = [];
+//         this._getProducts()
+//             .then(data => {
+//                 this.goods = [...data];
+//                 this.render()
+//             });
+//         this.render();
+//     }
+
+//     _getProducts(){
+//         return fetch(`${API}/catalogData.json`)
+//         .then(result =>  result.json())
+//         .catch(error => {
+//             console.log(error);
+//         });
+//     }
+
+//     getSum(){
+//         let sum = 0;
+
+//         for(let product of this.goods){
+//             sum += product.price;
+//         }   
+//         console.log(sum);
+//     }
+
+//     render(){
+//         const block = document.querySelector(this.container);
+//         for(let product of this.goods){
+//             const item = new ProductItem(product);
+//                 block.insertAdjacentHTML("beforeend", item.render());
+//         }
+//     }    
+// }
+
+
+// class ProductItem {
+//     constructor(product, img = 'img/ryba.jpg') {
+//         this.id = product.id_product;
+//         this.title = product.product_name;
+//         this.price = product.price;
+//         this.img = product.img;
+//     }
+
+//     render() {
+//         return `<div class="goods-item">
+//                 <img src="${this.img}" alt="ryba">
+//                 <h3>${this.title}</h3>
+//                 <p>${this.price}</p>
+//             <button class="buy-btn">Купить рыбки</button>           
+//         </div>`;
+//     }
+// }
+
+// let list = new ProductList();
+
+// list.render();
+// list.getSum();
